1539ff7a29ebbb131da9f313efaa0081435f7a97
Reinaldo <reinaldo.arifin.slamet@mhealth.tech>
2020-06-22 21:11:45 +0700

    add details for pokemon info
    add detail for pokemon info
    wip room

---
+++ app/src/main/java/com/explore/playground/repository/db/PokemonDatabase.kt
@@ -0,0 +1,34 @@
+package com.explore.playground.repository
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.explore.playground.repository.daos.PokemonURLDao
+import com.explore.playground.repository.model.PokemonURL
+
+@Database(entities = [PokemonURL::class], version = 1, exportSchema = true)
+abstract class PokemonDatabase : RoomDatabase() {
+    abstract fun pokemonDao(): PokemonURLDao
+
+    companion object {
+        @Volatile
+        var INSTANCE: PokemonDatabase? = null
+
+        fun getDatabase(ctx: Context): PokemonDatabase {
+            val tempInstance = INSTANCE
+            tempInstance?.let {
+                return it
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    ctx.applicationContext,
+                    PokemonDatabase::class.java,
+                    "poke_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/explore/playground/repository/db/daos/PokemonURLDao.kt
@@ -0,0 +1,18 @@
+package com.explore.playground.repository.daos
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.explore.playground.repository.model.PokemonURL
+import com.explore.playground.utils.LiveOnce
+
+interface PokemonURLDao {
+    @Query("SELECT * FROM pokemon_list")
+    fun getListPokemon(): LiveOnce<List<PokemonURL>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(pokemon: PokemonURL)
+
+    @Query("DELETE FROM pokemon_list")
+    fun delete()
+}
