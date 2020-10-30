6b310d9c7ba0d11e146583916995e1b1d65ec605
Reinaldo <reinaldo.arifin.slamet@mhealth.tech>
2020-10-30 19:08:26 +0700

    Refactor to SimpleCamera

--- app/src/main/java/com/explore/playground/easycamera/CameraNative.kt
+++ app/src/main/java/com/explore/playground/easycamera/CameraNative.kt
@@ -2,32 +2,26 @@ package com.explore.playground.easycamera
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
-import androidx.core.content.FileProvider
 import com.explore.playground.R
 import com.explore.playground.base.BaseActivity
+import com.explore.playground.utils.load
 import kotlinx.android.synthetic.main.activity_camera_native.*
-import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
 
 const val REQUEST_IMAGE_CAPTURE_BITMAP = 999
 const val REQUEST_IMAGE_CAPTURE = 9998
 
 class CameraNative : BaseActivity() {
-    lateinit var currentPhotoPath: String
+    lateinit var simpleCamera: SimpleCamera
     override fun setLayoutId(): Int {
         return R.layout.activity_camera_native
     }
 
     override fun setInit() {
+        simpleCamera = SimpleCamera(this)
         takePict.setOnClickListener {
-            dispatchTakePictureIntent()
+            simpleCamera.openCamera()
         }
     }
 
@@ -42,69 +36,18 @@ class CameraNative : BaseActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE_BITMAP && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            imagePict.setImageBitmap(imageBitmap)
-        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val files = File(currentPhotoPath)
-            
-        }
-    }
-
-    private fun dispatchTakePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            // Ensure that there's a camera activity to handle the intent
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                // Create the File where the photo should go
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    ex.printStackTrace()
-                    null
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    null
-                }
-                // Continue only if the File was successfully created
-                photoFile?.also {
-                    try {
-                        val photoURI: Uri = FileProvider.getUriForFile(
-                            this,
-                            "com.explore.playground.fileprovider",
-                            it
-                        )
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                    } catch (e: java.lang.Exception) {
-                        e.printStackTrace()
-                    }
-                }
-            }
-        }
-    }
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
+        simpleCamera.returnFile(requestCode, resultCode, data)?.let {
+            imagePict.load(it)
         }
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        currentPhotoPath = savedInstanceState.getString("Path", "")
+        simpleCamera.restorePath(savedInstanceState.getString("PathX", ""))
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString("Path", currentPhotoPath)
+        outState.putString("PathX", simpleCamera.getPath())
         super.onSaveInstanceState(outState)
     }
 }
---
+++ app/src/main/java/com/explore/playground/easycamera/SimpleCamera.kt
@@ -0,0 +1,102 @@
+package com.explore.playground.easycamera
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.fragment.app.FragmentActivity
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+const val CALL_SIMPLE_CAMERA = 21892
+
+class SimpleCamera(val activity: FragmentActivity) {
+    private var currentPhotoPath: String? = null
+    fun openCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(activity.applicationContext.packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    ex.printStackTrace()
+                    null
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    try {
+                        val photoURI: Uri = FileProvider.getUriForFile(
+                            activity.applicationContext,
+                            "com.explore.playground.fileprovider",
+                            it
+                        )
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                        activity.startActivityForResult(takePictureIntent, CALL_SIMPLE_CAMERA)
+                    } catch (e: java.lang.Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? =
+            activity.applicationContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    fun returnFile(requestCode: Int, resultCode: Int, data: Intent?): File? {
+        if (requestCode == CALL_SIMPLE_CAMERA && resultCode == AppCompatActivity.RESULT_OK) {
+            currentPhotoPath?.let {
+                return if (it.isNotEmpty()) {
+                    File(currentPhotoPath)
+                } else {
+                    Toast.makeText(
+                        activity.applicationContext,
+                        "Empty Photopath",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    null
+                }
+            } ?: kotlin.run {
+                Toast.makeText(
+                    activity.applicationContext,
+                    "Empty Photopath",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return null
+            }
+        } else {
+            return null
+        }
+    }
+
+    fun restorePath(path: String) {
+        currentPhotoPath = path
+    }
+
+    fun getPath(): String {
+        return currentPhotoPath ?: ""
+    }
+}
