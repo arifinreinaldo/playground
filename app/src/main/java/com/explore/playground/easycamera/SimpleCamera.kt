e89c8f31e46523e481f9147baba3c185491e27a7
Reinaldo <reinaldo.arifin.slamet@mhealth.tech>
2020-10-31 11:09:07 +0700

    add fragment / activity implementation

--- app/src/main/java/com/explore/playground/easycamera/SimpleCamera.kt
+++ app/src/main/java/com/explore/playground/easycamera/SimpleCamera.kt
@@ -1,5 +1,7 @@
 package com.explore.playground.easycamera
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Environment
@@ -7,7 +9,7 @@ import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.Fragment
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -16,12 +18,49 @@ import java.util.*
 
 const val CALL_SIMPLE_CAMERA = 21892
 
-class SimpleCamera(val activity: FragmentActivity, val provider: String) {
+class SimpleCamera {
+    private var call: ActivityCaller
+    private var fileProvider: String
+    private var context: Context
+
+    constructor(activity: Activity, provider: String) {
+        this.call = ActivityCaller(activity = activity)
+        this.fileProvider = provider
+        this.context = call.context
+    }
+
+    constructor(fragment: Fragment, provider: String) {
+        this.call = ActivityCaller(fragment = fragment)
+        this.fileProvider = provider
+        this.context = call.context
+    }
+
+    constructor(fragment: android.app.Fragment, provider: String) {
+        this.call = ActivityCaller(deprecatedFragment = fragment)
+        this.fileProvider = provider
+        this.context = call.context
+    }
+
+    private class ActivityCaller(
+        val fragment: Fragment? = null,
+        val activity: Activity? = null,
+        val deprecatedFragment: android.app.Fragment? = null
+    ) {
+        val context: Context
+            get() = (activity ?: fragment?.activity ?: deprecatedFragment?.activity)!!
+
+        fun startActivityForResult(intent: Intent, chooser: Int) {
+            activity?.startActivityForResult(intent, chooser)
+                ?: fragment?.startActivityForResult(intent, chooser)
+                ?: deprecatedFragment?.startActivityForResult(intent, chooser)
+        }
+    }
+
     private var currentPhotoPath: String? = null
     fun openCamera() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             // Ensure that there's a camera activity to handle the intent
-            takePictureIntent.resolveActivity(activity.applicationContext.packageManager)?.also {
+            takePictureIntent.resolveActivity(context.packageManager)?.also {
                 // Create the File where the photo should go
                 val photoFile: File? = try {
                     createImageFile()
@@ -36,12 +75,12 @@ class SimpleCamera(val activity: FragmentActivity, val provider: String) {
                 photoFile?.also {
                     try {
                         val photoURI: Uri = FileProvider.getUriForFile(
-                            activity.applicationContext,
-                            provider,
+                            context,
+                            fileProvider,
                             it
                         )
                         takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                        activity.startActivityForResult(takePictureIntent, CALL_SIMPLE_CAMERA)
+                        call?.startActivityForResult(takePictureIntent, CALL_SIMPLE_CAMERA)
                     } catch (e: java.lang.Exception) {
                         e.printStackTrace()
                     }
@@ -55,7 +94,7 @@ class SimpleCamera(val activity: FragmentActivity, val provider: String) {
         // Create an image file name
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val storageDir: File? =
-            activity.applicationContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
             "JPEG_${timeStamp}_", /* prefix */
             ".jpg", /* suffix */
@@ -73,7 +112,7 @@ class SimpleCamera(val activity: FragmentActivity, val provider: String) {
                     File(currentPhotoPath)
                 } else {
                     Toast.makeText(
-                        activity.applicationContext,
+                        context,
                         "Empty Photopath",
                         Toast.LENGTH_SHORT
                     ).show()
@@ -81,7 +120,7 @@ class SimpleCamera(val activity: FragmentActivity, val provider: String) {
                 }
             } ?: kotlin.run {
                 Toast.makeText(
-                    activity.applicationContext,
+                    context,
                     "Empty Photopath",
                     Toast.LENGTH_SHORT
                 ).show()
