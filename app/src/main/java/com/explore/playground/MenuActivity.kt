5a3bbc966cd8137788f0597c2c4fcbf60f8b3756
Reinaldo <reinaldo.arifin.slamet@mhealth.tech>
2020-12-03 13:57:28 +0700

    add share QR Image by whatsapp
    add QR library
    add generate QR image
    add share image to whatsapp

--- app/src/main/java/com/explore/playground/MenuActivity.kt
+++ app/src/main/java/com/explore/playground/MenuActivity.kt
@@ -1,7 +1,11 @@
 package com.explore.playground
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
 import android.util.Log
+import androidx.core.content.FileProvider
 import com.explore.playground.adapterAndRecyclerview.DummyRecyclerActivity
 import com.explore.playground.base.BaseActivity
 import com.explore.playground.base.BaseApp
@@ -21,7 +25,17 @@ import com.explore.playground.recorder.RecordingActivity
 import com.explore.playground.scrollAlpha.ScrollAlphaActivity
 import com.explore.playground.sociallogin.SocialLoginActivity
 import com.explore.playground.speechrecognition.SpeechRecogActivity
+import com.explore.playground.utils.load
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import kotlinx.android.synthetic.main.activity_menu.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MenuActivity : BaseActivity() {
     override fun setLayoutId(): Int {
@@ -87,6 +101,50 @@ class MenuActivity : BaseActivity() {
         }
         btCameraNative.setOnClickListener {
             startActivity(Intent(this, CameraNative::class.java))
+        }
+        btShare.setOnClickListener {
+            val text = "12321"
+            val format = MultiFormatWriter()
+            try {
+                val bitMatrix = format.encode(text, BarcodeFormat.QR_CODE, 200, 200)
+                val barcodeEncoder = BarcodeEncoder()
+                val bitmap = barcodeEncoder.createBitmap(bitMatrix)
+                val file = createImageFile()
+                val bos = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos)
+                val fos = FileOutputStream(file)
+                fos.write(bos.toByteArray())
+                fos.flush()
+                fos.close()
+                val photoURI: Uri = FileProvider.getUriForFile(
+                    this,
+                    "${BuildConfig.APPLICATION_ID}.fileprovider",
+                    file
+                )
+                ivImg.load(photoURI)
+                val whatsappIntent = Intent(Intent.ACTION_SEND)
+                whatsappIntent.type = "text/plain"
+                whatsappIntent.setPackage("com.whatsapp")
+                whatsappIntent.putExtra(Intent.EXTRA_TEXT, "The text you wanted to share")
+                whatsappIntent.putExtra(Intent.EXTRA_STREAM, photoURI)
+                whatsappIntent.type = "image/jpg"
+                whatsappIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivity(whatsappIntent)
+            } catch (e: Exception) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? =
+            this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
         }
     }
 }
